3cb777116e0024d6f1016b31df1e6b047bf16378
zzafergok <zfrgk96@gmail.com>
2020-09-03 20:11:26 +0300

    Scaffold body

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,36 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(new MaterialApp());
+void main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(primarySwatch: Colors.orange, accentColor: Colors.green),
+    home: new Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Flutter Dersleri",
+          style: TextStyle(fontSize: 18.0, color: Colors.white),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          debugPrint("FAB tıklandı");
+        },
+        child: Icon(
+          Icons.bookmark_border,
+          size: 32.0,
+          color: Colors.white,
+        ),
+      ),
+      body: Container(
+        color: Colors.lightBlue,
+        child: Center(
+          child: Text(
+            "Flutter Mükemmelmiş",
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    ),
+  ));
 }
