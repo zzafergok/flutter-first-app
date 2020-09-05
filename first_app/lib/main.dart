5147f4276048c0ca9452348e18438e1a654469f6
zzafergok <zfrgk96@gmail.com>
2020-09-05 18:54:20 +0300

    FadeInImage

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -5,114 +5,92 @@ void main() {
     debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.orange, accentColor: Colors.green),
     home: Scaffold(
-      appBar: AppBar(
-        title: Text(
-          "Flutter Dersleri",
-          style: TextStyle(fontSize: 24, color: Colors.white),
+        appBar: AppBar(
+          title: Text(
+            "Flutter Dersleri",
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
         ),
-      ),
 
-
-      // deneme örneği
-      body: Column(
-        children: <Widget>[
-          Text(
-            "Resim ve Buton Türleri",
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold
+        // deneme örneği
+        body: Column(
+          children: <Widget>[
+            Text(
+              "Resim ve Buton Türleri",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-          ),
-          IntrinsicHeight(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                Expanded(
-                  child: Container(
-                    color: Colors.red.shade300,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Image.network('https://picsum.photos/250?image=9'),
-                        Text("Merhaba"),
-                      ],
+            IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.red.shade300,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.network('https://picsum.photos/250?image=9'),
+                          Text("Network Image"),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.red.shade300,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Image.asset("assets/images/cat.jpg"),
-                        Text("Merhaba"),
-                      ],
+                  Expanded(
+                    child: Container(
+                      color: Colors.red.shade300,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          Image.asset("assets/images/cat.jpg"),
+                          Text("Asset Image"),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                Expanded(
-                  child: Container(
-                    color: Colors.red.shade300,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: NetworkImage('https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png'),
-                          backgroundColor: Colors.yellow.shade400,
-                          radius: 50,
-                          foregroundColor: Colors.green,
-                        ),
-                        Text("Merhaba"),
-                      ],
+                  Expanded(
+                    child: Container(
+                      color: Colors.red.shade300,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(
+                                'https://emrealtunbilek.com/wp-content/uploads/2016/10/apple-icon-72x72.png'),
+                            backgroundColor: Colors.yellow.shade400,
+                            radius: 50,
+                            foregroundColor: Colors.green,
+                          ),
+                          Text("Circle Image"),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              Container(
-                color: Colors.red.shade300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Image.asset("assets/images/cat.jpg", height: 75),
-                    Text("Merhaba"),
-                  ],
-                ),
-                margin: EdgeInsets.only(top:15),
-
+            IntrinsicHeight(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      color: Colors.red.shade300,
+                      height: 150,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          FadeInImage.assetNetwork(
+                              placeholder: "assets/images/loading.gif",
+                              image: 'https://picsum.photos/250?image=9', height: 75),
+                          Text("FadeInImage"),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              Container(
-                color: Colors.red.shade300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Image.asset("assets/images/cat.jpg", height: 75),
-                    Text("Merhaba"),
-                  ],
-                ),
-                margin: EdgeInsets.only(top:15),
-
-              ),
-              Container(
-                color: Colors.red.shade300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Image.asset("assets/images/cat.jpg", height: 75),
-                    Text("Merhaba"),
-                  ],
-                ),
-                margin: EdgeInsets.only(top:15),
-              ),
-            ],
-          )
-        ],
-      )
-    ),
+            ),
+          ],
+        )),
   ));
 }
