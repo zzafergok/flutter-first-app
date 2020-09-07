5ff7578037ae97b36fa889a2ffcb9f52d74461e0
zzafergok <zfrgk96@gmail.com>
2020-09-07 17:10:14 +0300

    Stateful Widget part one

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,52 @@
 import 'package:flutter/material.dart';
-import 'myapp.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      title: "MaterialApp Title",
+      home: MyHomePage(title: "My Home Page",),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+
+  MyHomePage({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => debugPrint("butona basılıyor"),
+        child: Icon(Icons.announcement),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Butona Basma Sayısı  :"),
+            Text(
+              "5",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
