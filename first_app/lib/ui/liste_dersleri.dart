e7700c0999313b28c949e00ad175014a9c6a850c
zzafergok <zfrgk96@gmail.com>
2020-09-07 22:51:44 +0300

    Basic use of Listview

--- first_app/lib/ui/liste_dersleri.dart
+++ first_app/lib/ui/liste_dersleri.dart
@@ -1,66 +1,58 @@
 import 'package:flutter/material.dart';
 
 class ListeDersleri extends StatelessWidget {
+  //Liste için veri kaynağı
+
+  List<int> listenumaralari = List.generate(300, (index) => index);
+  List<String> listeAltBasliklari =
+      List.generate(300, (index) => "Liste elemanı alt başlık $index");
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          child: Card(
-            color: Colors.blueGrey,
-            margin: EdgeInsets.all(20),
-            elevation: 20,
-            child: ListTile(
-              leading: CircleAvatar(child: Icon(Icons.android), radius: 20),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı altbaşlık"),
-              trailing: Icon(Icons.accessibility),
-            ),
+    return ListView(children: listenumaralari.map(
+          (oankiEleman) => Column(
+            children: [
+              Container(
+                child: Card(
+                  color: Colors.blueGrey,
+                  margin: EdgeInsets.all(20),
+                  elevation: 20,
+                  child: ListTile(
+                    leading:
+                        CircleAvatar(child: Icon(Icons.android), radius: 20),
+                    title: Text("Liste eleman başlık $oankiEleman"),
+                    subtitle: Text(listeAltBasliklari[oankiEleman]),
+                    trailing: Icon(Icons.accessibility),
+                  ),
+                ),
+              ),
+              Divider(color: Colors.red, height: 5, indent: 10, endIndent: 5),
+            ],
           ),
-        ),
-        Divider(color: Colors.red, height: 5, indent: 10, endIndent: 5),
-        Container(
-          child: Card(
-            color: Colors.blueGrey,
-            margin: EdgeInsets.all(20),
-            elevation: 20,
-            child: ListTile(
-              leading: CircleAvatar(child: Icon(Icons.android), radius: 20),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı altbaşlık"),
-              trailing: Icon(Icons.accessibility),
-            ),
-          ),
-        ),
-        Divider(color: Colors.red, height: 5, indent: 10, endIndent: 5),
-        Container(
-          child: Card(
-            color: Colors.blueGrey,
-            margin: EdgeInsets.all(20),
-            elevation: 20,
-            child: ListTile(
-              leading: CircleAvatar(child: Icon(Icons.android), radius: 20),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı altbaşlık"),
-              trailing: Icon(Icons.accessibility),
-            ),
-          ),
-        ),
-        Divider(color: Colors.red, height: 5, indent: 10, endIndent: 5),
-        Container(
-          child: Card(
-            color: Colors.blueGrey,
-            margin: EdgeInsets.all(20),
-            elevation: 20,
-            child: ListTile(
-              leading: CircleAvatar(child: Icon(Icons.android), radius: 20),
-              title: Text("Liste eleman başlık"),
-              subtitle: Text("Liste elemanı altbaşlık"),
-              trailing: Icon(Icons.accessibility),
-            ),
-          ),
-        ),
-      ],
+        ).toList(),
     );
   }
 }
+
+/**
+    children: [
+    Column(
+    children: [
+    Container(
+    child: Card(
+    color: Colors.blueGrey,
+    margin: EdgeInsets.all(20),
+    elevation: 20,
+    child: ListTile(
+    leading: CircleAvatar(child: Icon(Icons.android), radius: 20),
+    title: Text("Liste eleman başlık"),
+    subtitle: Text("Liste elemanı altbaşlık"),
+    trailing: Icon(Icons.accessibility),
+    ),
+    ),
+    ),
+    Divider(color: Colors.red, height: 5, indent: 10, endIndent: 5),
+    ],
+    ),
+    ],
+ */
