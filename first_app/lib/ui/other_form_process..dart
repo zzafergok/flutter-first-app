6de9944044ae18f3e5c84e6fb1591a47af7b9200
zzafergok <zfrgk96@gmail.com>
2020-09-17 22:50:46 +0300

    DropdownButton

--- first_app/lib/ui/other_form_process..dart
+++ first_app/lib/ui/other_form_process..dart
@@ -10,6 +10,9 @@ class _OtherFormProcessState extends State<OtherFormProcess> {
   String sehir = "";
   bool switchState = false;
   double sliderDeger = 10;
+  String secilenRenk = "Kirmizi";
+  String secilenSehir = "Ankara";
+  List<String> sehirler = ["Ankara", "İstanbul", "Amsterdam", "Amsterveen"];
 
   @override
   Widget build(BuildContext context) {
@@ -90,19 +93,87 @@ class _OtherFormProcessState extends State<OtherFormProcess> {
               subtitle: Text("merhaba ben subtitle"),
             ),
             Slider(
-                value: sliderDeger,
-                onChanged: (deger) {
-                  setState(() {
-                    sliderDeger = deger;
-                    debugPrint("Slider deger : $deger");
-                  });
-                },
+              value: sliderDeger,
+              onChanged: (deger) {
+                setState(() {
+                  sliderDeger = deger;
+                  debugPrint("Slider deger : $deger");
+                });
+              },
               min: 10,
               max: 30,
               divisions: 10,
               label: sliderDeger.toString(),
               activeColor: Colors.blue,
               //inactiveColor: Colors.orange,
+            ),
+            DropdownButton(
+              items: [
+                DropdownMenuItem<String>(
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        color: Colors.red,
+                        margin: EdgeInsets.only(right: 10),
+                      ),
+                      Text("Kirmizi"),
+                      //Container(child: Icon(Icons.add), margin: EdgeInsets.only(left: 15),),
+                    ],
+                  ),
+                  value: ("Kirmizi"),
+                ),
+                DropdownMenuItem<String>(
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        color: Colors.green,
+                        margin: EdgeInsets.only(right: 10),
+                      ),
+                      Text("Yeşil"),
+                    ],
+                  ),
+                  value: ("Yeşil"),
+                ),
+                DropdownMenuItem<String>(
+                  child: Row(
+                    children: [
+                      Container(
+                        width: 12,
+                        height: 12,
+                        color: Colors.blue,
+                        margin: EdgeInsets.only(right: 10),
+                      ),
+                      Text("Mavi"),
+                    ],
+                  ),
+                  value: ("Mavi"),
+                ),
+              ],
+              onChanged: (String secilen) {
+                setState(() {
+                  secilenRenk = secilen;
+                });
+              },
+              hint: Text("Seçiniz"),
+              value: secilenRenk,
+            ),
+            DropdownButton(
+                items: sehirler.map((oankiSehir) {
+                  return DropdownMenuItem(
+                    child: Text(oankiSehir),
+                    value: oankiSehir,
+                  );
+                }).toList(),
+                onChanged: (s) {
+                  setState(() {
+                    secilenSehir = s;
+                  });
+                },
+              value: secilenSehir,
             ),
           ],
         ),
