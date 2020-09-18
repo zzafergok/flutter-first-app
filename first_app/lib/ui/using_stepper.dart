129a6f1050896241bbf0f519fc7ebfbd4ec72bce
zzafergok <zfrgk96@gmail.com>
2020-09-18 14:46:45 +0300

    Using Stepper part two

--- first_app/lib/ui/using_stepper.dart
+++ first_app/lib/ui/using_stepper.dart
@@ -9,40 +9,65 @@ class _UsingStepperState extends State<UsingStepper> {
   int _aktifStep = 0;
   String isim, mail, sifre;
   List<Step> tumStepler;
+  bool hata = false;
+
+  var key0 = GlobalKey<FormFieldState>();
+  var key1 = GlobalKey<FormFieldState>();
+  var key2 = GlobalKey<FormFieldState>();
 
   @override
   void initState() {
+    // TODO: implement initState
     super.initState();
-    tumStepler=_tumStepler();
   }
 
   @override
   Widget build(BuildContext context) {
+    tumStepler = _tumStepler();
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("Using Stepper"),
+        title: Text('Stepper Örnek'),
       ),
       body: SingleChildScrollView(
         child: Stepper(
-          steps: tumStepler,
+          controlsBuilder: (BuildContext context,
+              {VoidCallback onStepContinue, VoidCallback onStepCancel}) {
+            return Row(
+              children: <Widget>[
+                RaisedButton(
+                  color: Colors.green,
+                  onPressed: onStepContinue,
+                  child: const Text('Devamm!'),
+                ),
+                SizedBox(
+                  width: 30,
+                ),
+                RaisedButton(
+                  color: Colors.pink,
+                  onPressed: onStepCancel,
+                  child: const Text('Geri Gel'),
+                ),
+              ],
+            );
+          },
           currentStep: _aktifStep,
-          onStepTapped: (tiklanilanStep) {
+          steps: tumStepler,
+          /* onStepTapped: (tiklanilanStep){
             setState(() {
               _aktifStep = tiklanilanStep;
             });
-          },
-          onStepContinue: (){
+          },*/
+          onStepContinue: () {
             setState(() {
-              if(_aktifStep < tumStepler.length-1){
-                _aktifStep++;
-              }
+              _ileriButonuKontrolu();
             });
           },
-          onStepCancel: (){
+          onStepCancel: () {
             setState(() {
-              if(_aktifStep > 0){
+              if (_aktifStep > 0) {
                 _aktifStep--;
-              }else{
+              } else {
                 _aktifStep = 0;
               }
             });
@@ -55,45 +80,120 @@ class _UsingStepperState extends State<UsingStepper> {
   List<Step> _tumStepler() {
     List<Step> stepler = [
       Step(
-        title: Text("Usernama title"),
-        subtitle: Text("Username subtitle"),
-        state: StepState.indexed,
+        title: Text("Username başlık"),
+        subtitle: Text("Username Altbaşlık"),
+        state: _stateleriAyarla(0),
         isActive: true,
         content: TextFormField(
+          key: key0,
           decoration: InputDecoration(
-            labelText: "Username label",
-            hintText: "UsernameHintText",
-            border: OutlineInputBorder(),
-          ),
+              labelText: "UsernameLabel",
+              hintText: "UsernameHint",
+              border: OutlineInputBorder()),
+          validator: (girilenDeger) {
+            if (girilenDeger.length < 6) {
+              return "En az 6 karakter giriniz";
+            }
+          },
+          onSaved: (girilenDeger) {
+            isim = girilenDeger;
+          },
         ),
       ),
       Step(
-        title: Text("Mail title"),
-        subtitle: Text("Mail subtitle"),
-        state: StepState.indexed,
+        title: Text("Mail başlık"),
+        subtitle: Text("Mail Altbaşlık"),
+        state: _stateleriAyarla(1),
         isActive: true,
         content: TextFormField(
+          key: key1,
           decoration: InputDecoration(
-            labelText: "Mail label",
-            hintText: "MailText",
-            border: OutlineInputBorder(),
-          ),
+              labelText: "MailLabel",
+              hintText: "MailHint",
+              border: OutlineInputBorder()),
+          validator: (girilenDeger) {
+            if (girilenDeger.length < 6 || !girilenDeger.contains("@")) {
+              return "Geçerli mail adresi giriniz";
+            }
+          },
+          onSaved: (girilenDeger) {
+            mail = girilenDeger;
+          },
         ),
       ),
       Step(
-        title: Text("Password title"),
-        subtitle: Text("Password subtitle"),
-        state: StepState.indexed,
+        title: Text("Şifre başlık"),
+        subtitle: Text("Şifre Altbaşlık"),
+        state: _stateleriAyarla(2),
         isActive: true,
         content: TextFormField(
+          key: key2,
           decoration: InputDecoration(
-            labelText: "Password label",
-            hintText: "PasswordText",
-            border: OutlineInputBorder(),
-          ),
+              labelText: "ŞifreLabel",
+              hintText: "ŞifreHint",
+              border: OutlineInputBorder()),
+          validator: (girilenDeger) {
+            if (girilenDeger.length < 6) {
+              return "Şifre En az 6 karakter olmalı";
+            }
+          },
+          onSaved: (girilenDeger) {
+            sifre = girilenDeger;
+          },
         ),
       ),
     ];
+
     return stepler;
+  }
+
+  StepState _stateleriAyarla(int oankiStep) {
+    if (_aktifStep == oankiStep) {
+      if (hata) {
+        return StepState.error;
+      } else {
+        return StepState.editing;
+      }
+    } else
+      return StepState.complete;
+  }
+
+  void _ileriButonuKontrolu() {
+    switch (_aktifStep) {
+      case 0:
+        if (key0.currentState.validate()) {
+          key0.currentState.save();
+          hata = false;
+          _aktifStep = 1;
+        } else {
+          hata = true;
+        }
+        break;
+
+      case 1:
+        if (key1.currentState.validate()) {
+          key1.currentState.save();
+          hata = false;
+          _aktifStep = 2;
+        } else {
+          hata = true;
+        }
+        break;
+
+      case 2:
+        if (key2.currentState.validate()) {
+          key2.currentState.save();
+          hata = false;
+          _aktifStep = 2;
+          formTamamlandi();
+        } else {
+          hata = true;
+        }
+        break;
+    }
+  }
+
+  void formTamamlandi() {
+    debugPrint("Girilen değerler : isim=>$isim mail=>$mail şifre=>$sifre");
   }
 }
